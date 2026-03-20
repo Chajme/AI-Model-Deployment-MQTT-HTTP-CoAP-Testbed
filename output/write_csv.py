b0f119e68e9040b1116de4cf1a523361ce93ddcf
Chajme <erikmolitoris60@gmail.com>
2026-03-20 11:31:00 +0100

    MQTT time to send measurements for different QoS levels - binary files get sent - time it takes to send and receive gets measured - measurements get saved to a .csv file

--- output/write_csv.py
+++ output/write_csv.py
@@ -2,12 +2,26 @@ import csv
 import os
 
 
-def write_to_file(data: list[dict]):
-    output_path = "/app/output/protocols_measurements.csv"
+def write_to_file_http(data: list[dict]):
+    output_path = "/app/output/http_measurements.csv"
     file_exists = os.path.isfile(output_path)
 
     with open(output_path, 'a', newline='') as csvfile:
         fieldnames = ['protocol', 'filesize', 'time_to_transfer']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if not file_exists:
+            writer.writeheader()
+
+        writer.writerows(data)
+        print(f"Writing to {output_path}")
+
+def write_to_file_mqtt(data: list[dict]):
+    output_path = "/app/output/mqtt_measurements.csv"
+    file_exists = os.path.isfile(output_path)
+
+    with open(output_path, 'a', newline='') as csvfile:
+        fieldnames = ['protocol', 'qos', 'side', 'file_size', 'sender_duration', 'receiver_duration']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         if not file_exists:
